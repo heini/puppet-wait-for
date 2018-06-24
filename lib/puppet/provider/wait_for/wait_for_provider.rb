b1668fe0d2082e30071ca838c84923798268bc52
Alex Harvey <alex@razorconsulting.com.au>
2018-06-25 01:11:52 +1000

    Clean up in wait_for_provider.rb
    Clean up for more consistent use of fetch_parameters, and refactor some
    duplication, and change from Python- to Ruby-style for loop.

--- lib/puppet/provider/wait_for/wait_for_provider.rb
+++ lib/puppet/provider/wait_for/wait_for_provider.rb
@@ -2,35 +2,35 @@ Puppet::Type.type(:wait_for).provide(:wait_for) do
   desc "Waits for something to happen."
 
   def exit_code
-    query = resource[:query]
+    fetch_parameters
     set_environment
-    `#{query}`
+    `#{@query}`
     return $?
   end
 
-  def exit_code=(expected_exit_code)
-    fetch_parameters
-    info "waiting until the exit code of #{@query} is #{expected_exit_code.to_s}"
+  def exit_code=(expected)
+    info "waiting until the exit code of #{@query} is #{expected}"
     info "polling frequency #{@polling_frequency}, max retries #{@max_retries}"
-    error_message = "wait_for timed out while waiting until the exit code of #{@query} became #{expected_exit_code}, after #{@max_retries} retries with polling frequency #{@polling_frequency}."
-    query_and_wait(error_message) do |exit_code, output|
-      if expected_exit_code == exit_code
+
+    message = set_message("the exit code of #{@query} became #{expected}")
+
+    query_and_wait(message) do |exit_code, output|
+      if expected == exit_code
         info "Detected expected exit code."
-        return expected_exit_code
+        return expected
       else
-        debug "Exit code is #{exit_code} but we are waiting for #{expected_exit_code}."
+        debug "Exit code is #{exit_code} but we are waiting for #{expected}."
       end
     end
   end
 
   def regex
-    query = resource[:query]
-    regex = resource[:regex]
+    fetch_parameters
     set_environment
-    output = `#{query}`
-    if output =~ regex
+    output = `#{@query}`
+    if output =~ @regex
       info "Query output matched regex."
-      return regex
+      return @regex
     else
       return nil
     end
@@ -38,57 +38,69 @@ Puppet::Type.type(:wait_for).provide(:wait_for) do
 
   def regex=(regex)
     fetch_parameters
-    info "waiting until the output of #{@query} matches #{regex.to_s}"
+
+    info "waiting until the output of #{@query} matches #{regex}"
     info "polling frequency #{@polling_frequency}, max retries #{@max_retries}"
-    error_message = "wait_for timed out while waiting until the output of #{@query} matched #{regex}, after #{@max_retries} retries with polling frequency #{@polling_frequency}."
-    query_and_wait(error_message) do |exit_code, output|
+
+    message = set_message("the output of #{@query} matched #{regex}")
+
+    query_and_wait(message) do |exit_code, output|
       if output =~ regex
         info "Query output matched regex."
         return regex
       else
-        debug "Query output #{output} did not match regex #{regex.to_s}."
+        debug "Query output #{output} did not match regex #{regex}."
       end
     end
   end
 
   def seconds
-    seconds = resource[:seconds]
-    info "Waiting for #{seconds} seconds..."
-    sleep(seconds)
-    return seconds
+    fetch_parameters
+    info "Waiting for #{@seconds} seconds..."
+    sleep(@seconds)
+    return @seconds
   end
 
-  private
+ private
 
   def fetch_parameters
-    @query             = resource[:query]
+    @query    = resource[:query]
+    @regex    = resource[:regex]
+    @seconds  = resource[:seconds]
+    @environment = resource[:environment]
+    @max_retries = resource[:max_retries]
     @polling_frequency = resource[:polling_frequency]
-    @max_retries       = resource[:max_retries]
   end
 
   def set_environment
-    environment = resource[:environment]
-    environment.each do |item|
-      (key, value) = item.split('=')
+    @environment.each do |item|
+      key, value = item.split('=')
       debug "Setting environment variable #{key}."
       ENV[key] = value
     end
   end
 
-  def query_and_wait(error_message)
-    set_environment
-    for i in 1..@max_retries
-      debug "Retry #{i}"
+  def set_message(reason)
+    return "wait_for timed out while waiting until #{reason}, " \
+      "after #{@max_retries} retries " \
+      "with polling frequency #{@polling_frequency}."
+  end
+
+  def query_and_wait(message)
+    1.upto(@max_retries) do |i|
+
       output    = `#{@query}`
       exit_code = $?
-      debug "exit code: #{exit_code.to_s}"
-      debug "output: #{output.to_s}"
+
+      debug "Retry #{i}"
+      debug "exit code: #{exit_code}"
+      debug "output: #{output}"
 
       yield(exit_code, output)
 
       sleep @polling_frequency
     end
 
-    fail error_message
+    fail message
   end
 end
