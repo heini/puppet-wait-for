7a3a67706ace52a9509d3c9a1cd751c952f38408
Alex Harvey <alexharv074@gmail.com>
2019-11-05 13:00:53 +1100

    Add test cases for refreshonly for exit_code and query

--- spec/acceptance/wait_for_spec.rb
+++ spec/acceptance/wait_for_spec.rb
@@ -68,7 +68,32 @@ describe 'wait_for' do
       }
     EOF
     expect(apply_manifest(pp).stdout).to match(/Wait_for.*Triggered 'refresh' from 1 event/)
-    expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero # Idempotence- nothing should happen on the second run!
+    expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero # Idempotence - nothing should happen on the second run.
+  end
+
+  it 'should not error out after max_retries if exit code is wrong if refreshonly' do
+    pp = <<~EOF
+      wait_for { '/usr/bin/false':
+        exit_code         => 2,
+        polling_frequency => 0.3,
+        max_retries       => 5,
+        refreshonly       => true,
+      }
+    EOF
+    expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero # Nothing should happen.
+  end
+
+  it 'should not error out after max_retries if regex never matches if refreshonly' do
+    pp = <<~EOF
+      wait_for { 'unicorns':
+        query             => 'cat /var/log/messages',
+        regex             => 'I am a pattern that should never be seen in /var/log/messages',
+        polling_frequency => 5,
+        max_retries       => 2,
+        refreshonly       => true,
+      }
+    EOF
+    expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
   end
 
   it 'should inject variables' do
